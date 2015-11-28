8272a9740adb88be3d73d0503ab81bd2f31dda55
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-11-28 16:21:37 +0100

    Fixing SEO not refreshing well

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -13,4 +13,21 @@ Template.home.onCreated(function()
 			'property="og:image"': 'og.png' //OG IMAGE HERE
 		}
 	});
-})
+
+	setTimeout(function()
+	{
+		SEO.set(
+		{
+			title: orion.dictionary.get('seo.title', 'Themia.co'),
+			description: orion.dictionary.get('seo.description'),
+			meta: 
+			{
+				'property="og:type"': 'website',
+				'property="og:title"': orion.dictionary.get('seo.title', 'Themia.co'),
+				'property="og:description"': orion.dictionary.get('seo.description'),
+				'property="og:url"': FlowRouter.url(''),
+				'property="og:image"': 'og.png' //OG IMAGE HERE
+			}
+		});
+	}, 500);
+});
