dc78f51ec58b741ee9024df1f867c52f91ef633d
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-11-28 18:12:34 +0100

    Now wait for subscriptions (and fix bugs)

--- client/views/theme/theme.js
+++ client/views/theme/theme.js
@@ -1,41 +1,42 @@
 Template.theme.helpers({
 	theme: function()
 	{
-		return Session.get('theme');
+		var cat = Categories.findOne({canonicalName: FlowRouter.current().params.category});
+
+		if(!cat)
+		{
+			return FlowRouter.go('/404');
+		}
+
+		var theme = Themes.findOne({category: cat._id, canonicalName: FlowRouter.current().params.name});
+
+		if(!theme)
+		{
+			return FlowRouter.go('/404');
+		}
+
+		var seoTitle = theme.title + ' theme for ' + cat.name;
+		var seoDescription = theme.description.slice(0, 155);
+
+		SEO.set(
+		{
+			title: seoTitle,
+			description: seoDescription,
+			meta:
+			{
+				'property="og:title"': seoTitle,
+				'property="og:description"': seoDescription,
+				'property="og:url"': FlowRouter.url(FlowRouter.current().path),
+				'property="og:image"': theme.image ? theme.image.url : ''
+			}
+		});
+
+		return theme;
 	}
 });
 
 Template.theme.onCreated(function()
 {
-	var cat = Categories.findOne({canonicalName: FlowRouter.current().params.category});
-
-	if(!cat)
-	{
-		FlowRouter.go('/404');
-	}
-
-	var theme = Themes.findOne({category: cat._id, canonicalName: FlowRouter.current().params.name});
-
-	if(!theme)
-	{
-		FlowRouter.go('/404');
-	}
-
-	Session.set('theme', theme);
-
-	var seoTitle = theme.title;
-	var seoDescription = theme.description.slice(0, 155);
-
-	SEO.set(
-	{
-		title: seoTitle,
-		description: seoDescription,
-		meta:
-		{
-			'property="og:title"': seoTitle,
-			'property="og:description"': seoDescription,
-			'property="og:url"': FlowRouter.url(FlowRouter.current().path),
-			'property="og:image"': theme.image ? theme.image.url : ''
-		}
-	});
+	this.subscribe('categories');
+	this.subscribe('themes');
 });
--- client/views/thumbnails/thumbnails.js
+++ client/views/thumbnails/thumbnails.js
@@ -1,13 +1,27 @@
-Template.thumbnails.helpers({
-  themes: function(){
-    console.log("Inside theme helper.");
-
+Template.thumbnails.helpers(
+{
+  themes: function()
+  {
     var themes = Themes.find();
-    console.log('There is ' + themes.count() + ' themes.');
+
+    SEO.set(
+    {
+      title: orion.dictionary.get('seo.title'),
+      description: orion.dictionary.get('seo.description'),
+      meta: 
+      {
+        'property="og:type"': 'website',
+        'property="og:title"': orion.dictionary.get('seo.title'),
+        'property="og:description"': orion.dictionary.get('seo.description'),
+        'property="og:url"': FlowRouter.url(''),
+        'property="og:image"': 'og.png' //OG IMAGE HERE
+      }
+    });
 
     return themes;
   },
-  'themeUrl': function(catId, theme){
+  'themeUrl': function(catId, theme)
+  {
   	if(!catId || !theme)
   		return;
 
@@ -18,4 +32,10 @@ Template.thumbnails.helpers({
 
   	return FlowRouter.url('/' + cat.canonicalName + '/' + theme);
   }
+});
+
+Template.thumbnails.onCreated(function()
+{
+  this.subscribe('themes');
+  this.subscribe('categories');
 });
